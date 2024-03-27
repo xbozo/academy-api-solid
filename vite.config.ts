0f28cf5bc5f72db15c5d2539f5192d8026c5705d
Guilherme Viana <imluizguilara@hotmail.com>
2024-03-27 13:45:32 -0300

    feat: create prisma e2e test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [tsConfigPaths()],
+	test: {
+		environmentMatchGlobs: [
+			['src/http/controllers/**', './prisma/vitest-environment-prisma/prisma-test-environment.ts'],
+		],
+		dir: 'src',
+	},
 })
